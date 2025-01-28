bfc4ec33c92b45950ede4052d6eb8fbe1c824a5f
Matt Gaidica, Ph.D. <matt@gaidi.ca>
2025-01-28 11:13:35 -0600

    Update BasicLoggingHublink.ino

--- examples/BasicLoggingHublink/BasicLoggingHublink.ino
+++ examples/BasicLoggingHublink/BasicLoggingHublink.ino
@@ -3,43 +3,36 @@
 
 HublinkBEAM beam;
 Hublink hublink(PIN_SD_CS);
-const unsigned long LOG_EVERY_MINUTES = 10;  // Log every X minutes
-const unsigned long SYNC_EVERY_MINUTES = 30; // Sync every X minutes
-const unsigned long SYNC_FOR_SECONDS = 30;   // Sync timeout in seconds
+const unsigned long LOG_EVERY_MINUTES = 10;   // Log every X minutes
+const unsigned long SYNC_EVERY_MINUTES = 60;  // Sync every X minutes
+const unsigned long SYNC_FOR_SECONDS = 30;    // Sync timeout in seconds
 
-void setup()
-{
+void setup() {
   // Configure behavior
-  beam.setNewFileOnBoot(false); // false to continue using same file if it's the same day
-  beam.setInactivityPeriod(40); // 40 seconds; based on https://shorturl.at/JiZxK
+  beam.setNewFileOnBoot(true);   // false to continue using same file if it's the same day
+  beam.setInactivityPeriod(40);  // 40 seconds; based on https://shorturl.at/JiZxK
 
   // Wait for the beam to initialize, retry (likely due to SD card ejecting)
-  while (!beam.begin())
-  {
-    delay(1000); // Wait 1 second before retrying
+  while (!beam.begin()) {
+    delay(1000);  // Wait 1 second before retrying
   }
 }
 
-void loop()
-{
+void loop() {
   beam.logData();
 
   // Check if interval has passed (and set up alarm on first run)
-  if (beam.alarm(SYNC_EVERY_MINUTES))
-  {
+  if (beam.alarm(SYNC_EVERY_MINUTES)) {
     Serial.println("Alarm triggered!");
 
     // only begin when alarm is triggered (risks not catching error at setup though)
-    if (hublink.begin())
-    {
+    if (hublink.begin()) {
       Serial.println("✓ Hublink.");
 
       // this is a forced sync, so it will ignore some of the settings in meta.json
       // such as: advertise_every, advertise_for, or try_reconnect
       hublink.sync(SYNC_FOR_SECONDS);
-    }
-    else
-    {
+    } else {
       Serial.println("✗ Failed.");
     }
   }
@@ -52,5 +45,5 @@ void loop()
    * will restart after deep sleep, returning to setup(); nothing beyond beam.sleep()
    * will be executed.
    */
-  beam.sleep(LOG_EVERY_MINUTES); // Sleep for LOG_EVERY_MINUTES minutes
+  beam.sleep(LOG_EVERY_MINUTES);  // Sleep for LOG_EVERY_MINUTES minutes
 }
